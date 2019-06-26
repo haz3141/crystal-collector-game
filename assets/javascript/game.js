0ad3100cb65e5c17edea72a87d5bdac624211fc9
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-06-26 15:03:11 -0400

    added random number generation for target and crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,40 @@ $(document).ready(function() {
 
     // Global Variables
     let counter = 0;
-    let targetNum = 50;
+    let targetNum = 0;
+    let crystalNums = [];
 
-    $('#number-to-guess').text(targetNum);
+    function newGame() {
+
+        // Generate new random target and write to page
+        targetNum = getRndInteger(19, 120);
+        $('#number-to-guess').text(targetNum);
+
+        // Generate four random numbers for crystals
+        for (let i = 0; i < 4; i++) {
+            crystalNums[i] = getRndInteger(1, 12);
+        };
+    };
+
+    
 
     $('.crystal-image').click(function() {
         
-        counter += 10;
+        counter += crystalNums[0];
         alert('Your new score is: ' + counter);
 
         if (counter === targetNum) {
             alert('You Win!');
+        } else if (counter > targetNum) {
+            alert('You Lose!');
         };
     });
 
+    // Random Number Function inclusive of min and max
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    };
+
+    // Begin new game
+    newGame();
 });
