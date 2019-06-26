b93ef6851487f21760e7d6c3044749e7e02993d3
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-06-26 14:07:58 -0400

    write target number to the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 
     // Global Variables
     let counter = 0;
+    let targetNum = 50;
+
+    $('#number-to-guess').text(targetNum);
 
     $('.crystal-image').click(function() {
         counter += 1;
